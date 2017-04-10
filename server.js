c7598710f65e2ec42f7c6fbbe282ac9d4f522f53
andela-aonifade <anuoluwapo.onifade@andela.com>
2017-04-10 17:02:28 +0100

    Updates the server.js to serve the index.html

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const connect = require('connect');
 const serveStatic = require('serve-static');
 
 const port = process.env.PORT || 3000;
-connect().use(serveStatic('./src')).listen(port);
+connect().use(serveStatic('./')).listen(port);
